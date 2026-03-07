a4c47702f59989fef09317b2795b8bc00569b062
yuyui-tw <yuyuitw62@gmail.com>
2026-03-07 21:58:14 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "yuyui-tw.github.io/NotesPublic/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
